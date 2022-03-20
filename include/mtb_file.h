815552a653e74eb80f319c91ba7951582f92f251
Murilo Baldi <mb@murilobaldi.com>
2022-03-20 11:04:40 +0100

    Add __cplusplus guard on header files

--- include/mtb_file.h
+++ include/mtb_file.h
@@ -3,8 +3,16 @@
 
 #include <stdio.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char *mtb_file_readline(FILE *);
 char **mtb_file_readall(FILE *, int *);
 char **mtb_filename_readall(char *, int *);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- include/mtb_str.h
+++ include/mtb_str.h
@@ -3,11 +3,19 @@
 
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 char *mtbs_new(char *);
 char *mtbs_join(int, char *, ...);
 void mtbs_concat(int, char **, ...);
 char *mtbs_substr(char *, size_t, size_t);
 char **mtbs_split(char *, char *, int *);
 void mtbs_split_free(char **, int);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
